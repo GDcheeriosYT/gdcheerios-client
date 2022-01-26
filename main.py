8bfa63cd005e63661c06156306155595f06d359e
GDcheerios <minecreeper0913@gmail.com>
2022-01-26 17:29:08 -0600

    only refresh player score once after score submission

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ print("I use this for data reading, so thanks to him")
 os.startfile("gosumemory.exe")
 
 time.sleep(5)
+completed = False #boolean represents if map is completed or not
 
 while True:
   info = requests.get("http://127.0.0.1:24050/json").json()
@@ -30,19 +31,24 @@ while True:
     important_info["state"] = info['menu']['state']
     important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
     requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
+    completed = False
     time.sleep(1)
   elif info['menu']['state'] == 7:
-    requests.get(f"http://gdcheerios.com/refresh/{id}")
-    important_info = {}
-    important_info["mapInfo"] = {"background" : f"https://assets.ppy.sh/beatmaps/{info['menu']['bm']['set']}/covers/cover.jpg","metadata" : info['menu']['bm']['metadata'],"stats" : info['menu']['bm']['stats'],"mods" : info['menu']['mods']["str"]}
-    important_info["state"] = info['menu']['state']
-    important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
-    requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
-    time.sleep(6)
+    if completed == False:
+      requests.get(f"http://gdcheerios.com/refresh/{id}")
+      important_info = {}
+      important_info["mapInfo"] = {"background" : f"https://assets.ppy.sh/beatmaps/{info['menu']['bm']['set']}/covers/cover.jpg","metadata" : info['menu']['bm']['metadata'],"stats" : info['menu']['bm']['stats'],"mods" : info['menu']['mods']["str"]}
+      important_info["state"] = info['menu']['state']
+      important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
+      requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
+      completed = True
+    time.sleep(2)
+    
   else:
     important_info = {}
     important_info["mapInfo"] = {"background" : f"https://assets.ppy.sh/beatmaps/{info['menu']['bm']['set']}/covers/cover.jpg","metadata" : info['menu']['bm']['metadata'],"stats" : info['menu']['bm']['stats'],"mods" : info['menu']['mods']["str"]}
     important_info["state"] = info['menu']['state']
     important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
     requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
-    time.sleep(0.35)
+    completed = False
+    time.sleep(1)
