80becd19a2fd3d9f1d327f574c348b3940b48b5b
GDcheerios <minecreeper0913@gmail.com>
2022-01-27 03:20:27 -0600

    added global delay

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from importlib.metadata import metadata
 import time
-import json
-from flask import jsonify
 import requests
-import subprocess
 import os
 import atexit
 
@@ -11,6 +7,8 @@ def exit_handler():
     requests.post(f"http://gdcheerios.com/api/live/del/{id}")
 
 atexit.register(exit_handler)
+
+delay = 1.9
 
 with open("id.txt", "r") as f:
   id = f.read()
@@ -32,7 +30,7 @@ while True:
     important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
     requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
     completed = False
-    time.sleep(1)
+    time.sleep(delay)
   elif info['menu']['state'] == 7:
     if completed == False:
       requests.get(f"http://gdcheerios.com/refresh/{id}")
@@ -42,7 +40,7 @@ while True:
       important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
       requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
       completed = True
-    time.sleep(2)
+    time.sleep(delay)
     
   else:
     important_info = {}
@@ -51,4 +49,4 @@ while True:
     important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
     requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
     completed = False
-    time.sleep(1)
+    time.sleep(delay)
