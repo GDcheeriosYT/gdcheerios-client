b8cbd8690d856a3decf42a10fe5d5b8a342ea2cf
GDcheerios <minecreeper0913@gmail.com>
2022-01-31 23:07:40 -0600

    got rid of lists

--- main.py
+++ main.py
@@ -17,11 +17,6 @@ except:
 
 atexit.register(exit_handler)
 
-delay = 2
-
-score = [0]
-combo = [0]
-accuracy = [0]
 
 with open("id.txt", "r") as f:
   id = f.read()
@@ -35,25 +30,15 @@ time.sleep(5)
 completed = False #boolean represents if map is completed or not
 
 while True:
-  print(score[len(score)-1])
-  print(combo[len(combo)-1])
-  print(accuracy[len(accuracy)-1])
   info = requests.get("http://127.0.0.1:24050/json").json()
   if info['menu']['state'] != 2 and info['menu']['state'] != 7:
     important_info = {}
     important_info["mapInfo"] = {"background" : f"https://assets.ppy.sh/beatmaps/{info['menu']['bm']['set']}/covers/cover.jpg","metadata" : info['menu']['bm']['metadata'],"stats" : info['menu']['bm']['stats'],"mods" : info['menu']['mods']["str"]}
     important_info["state"] = info['menu']['state']
     important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
-    score.append(info['gameplay']['score'])
-    combo.append(info['gameplay']['combo']['current'])
-    accuracy.append(info['gameplay']['accuracy'])
-    important_info["lists"] = {"combo" : combo, "accuracy" : accuracy, "score" : score}
     requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
     completed = False
     time.sleep(delay)
-    score = [0]
-    combo = [0]
-    accuracy = [0]
     
   elif info['menu']['state'] == 7:
     if completed == False:
@@ -62,26 +47,15 @@ while True:
       important_info["mapInfo"] = {"background" : f"https://assets.ppy.sh/beatmaps/{info['menu']['bm']['set']}/covers/cover.jpg","metadata" : info['menu']['bm']['metadata'],"stats" : info['menu']['bm']['stats'],"mods" : info['menu']['mods']["str"]}
       important_info["state"] = info['menu']['state']
       important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
-      score.append(info['gameplay']['score'])
-      combo.append(info['gameplay']['combo']['current'])
-      accuracy.append(info['gameplay']['accuracy'])
-      important_info["lists"] = {"combo" : combo, "accuracy" : accuracy, "score" : score}
       requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
       completed = True
     time.sleep(delay)
-    score = [0]
-    combo = [0]
-    accuracy = [0]
     
   else:
     important_info = {}
     important_info["mapInfo"] = {"background" : f"https://assets.ppy.sh/beatmaps/{info['menu']['bm']['set']}/covers/cover.jpg","metadata" : info['menu']['bm']['metadata'],"stats" : info['menu']['bm']['stats'],"mods" : info['menu']['mods']["str"]}
     important_info["state"] = info['menu']['state']
     important_info["gameplay"] = {"score" : info['gameplay']['score'],"accuracy" : info['gameplay']['accuracy'],"combo" : info['gameplay']['combo']['current'], "grade":info["gameplay"]["hits"]["grade"]["current"]}
-    score.append(info['gameplay']['score'])
-    combo.append(info['gameplay']['combo']['current'])
-    accuracy.append(info['gameplay']['accuracy'])
-    important_info["lists"] = {"combo" : combo, "accuracy" : accuracy, "score" : score}
     requests.post(f"http://gdcheerios.com/api/live/update/{id}", json=important_info)
     completed = False
     time.sleep(delay)
