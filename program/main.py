3081e782f7b83db66e4625fcac654ce4dbcdd500
GDcheerios <minecreeper0913@gmail.com>
2022-02-21 16:22:10 -0600

    added stream overlay option

--- program/main.py
+++ program/main.py
@@ -1,32 +1,28 @@
 import time
 import os
 import atexit
-import requests
-import psutil
+from turtle import width
 from tkinter import *
 import osu_refresher
-    
-def get_pid(program_name):
-  for proc in psutil.process_iter():
-    if proc.name() == program_name:
-      print(proc)
-      print(proc.pid)
-      return(proc.pid)
-    
-
-def start_osu_refresher():
-  osu_refresher.prepare()
-  osu_refresher.credits()
-  osu_refresher.request_loop()
-
 
 
 window = Tk()
 app = window
-#buttons
 
+#functions
+def start_osu_refresher():
+  osu_refresher.prepare()
+  osu_refresher.credits() 
+  osu_refresher.request_loop()
+
+def start_stream_overlay():
+  print("Coming Soon...")
+
+#buttons
 start_osu = Button(text="osu", command=start_osu_refresher)
 start_osu.place(x=0, y=0)
+start_overlay = Button(text="stream overlay", command=start_stream_overlay)
+start_overlay.place(x=100, y=0)
   
 window.title("gdcheerios.com client")
 window.geometry("320x320")
